77df2fdd90948336a91ce0ab8006465cfce03cbd
Vincent <vincentli.dev@gmail.com>
2024-12-05 19:13:24 -0500

    fixed auth file

--- front-end/app/context/AuthContext.tsx
+++ front-end/app/context/AuthContext.tsx
@@ -1,23 +1,37 @@
 'use client'
 
-import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useContext, useState, useEffect } from 'react';
 
 const AuthContext = createContext(null);
 
 export const AuthProvider = ({ children }) => {
   const [isLoggedIn, setIsLoggedIn] = useState(() => {
-    return localStorage.getItem('isLoggedIn') === 'true';
+    if (typeof window !== 'undefined') {
+      return localStorage.getItem('isLoggedIn') === 'true';
+    }
+    return false;
   });
 
   const login = () => {
-    localStorage.setItem('isLoggedIn', 'true');
     setIsLoggedIn(true);
+    if (typeof window !== 'undefined') {
+      localStorage.setItem('isLoggedIn', 'true');
+    }
   };
 
   const logout = () => {
-    localStorage.removeItem('isLoggedIn');
     setIsLoggedIn(false);
+    if (typeof window !== 'undefined') {
+      localStorage.removeItem('isLoggedIn');
+    }
   };
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const loggedInStatus = localStorage.getItem('isLoggedIn') === 'true';
+      setIsLoggedIn(loggedInStatus);
+    }
+  }, []);
 
   return (
     <AuthContext.Provider value={{ isLoggedIn, login, logout }}>
